aefb437162b672e1c709668fc7e364746cec5ef6
koppalshashi <koppalshashikumar2125@gmail.com>
2025-08-09 12:01:27 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/vote", require("./routes/voteRoutes"));
+app.use("/api/results", require("./routes/resultRoutes"));
+app.use("/api/candidates", require("./routes/candidateRoutes")); 
+app.use("/api/form-sync", require("./routes/formSyncRoutes"));
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+
